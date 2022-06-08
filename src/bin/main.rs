0fa0fe29981a1a19fd3e4c81d87e8ab5e95dcf92
Robert Cunningham <robertvcunningham@gmail.com>
2022-06-07 20:02:24 -0400

    board working

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,15 +1,12 @@
+use std::error::Error;
+
 use regex::Regex;
 
 fn main() {
     println!("Hello, world!");
 }
 
-// ultimately exposes things like get white pieces, get white possible moves.
-
-
-struct Move {}
-
-
+#[derive(Clone, Copy, PartialEq, Debug)]
 enum PieceType {
     Pawn,
     Bishop,
@@ -19,17 +16,14 @@ enum PieceType {
     Queen,
 }
 
+#[derive(Clone, Copy, PartialEq, Debug)]
 struct Piece {
     pieceType: PieceType,
     color: Color,
 }
 
+#[derive(Clone, Copy, PartialEq, Debug)]
 enum Color {
-    White,
-    Black
-}
-
-enum Player {
     White,
     Black
 }
@@ -39,24 +33,35 @@ struct Location {
     y: i8
 }
 
+impl Location {
+    fn new(x: i8, y: i8) -> Self {
+        Location{x, y}
+    }
+}
+
 struct Game {
+    board: ArrayBoard,
 }
 
 impl Game {
     fn new() -> Game {
-        return Game{}
+        return Game{board: Board::new()}
     }
 
     fn get_possible_moves(p: Player) {
 
     }
 
-
     // 8/5k2/3p4/1p1Pp2p/pP2Pp1P/P4P1K/8/8 b - - 99 50
-    fn from_fen(s: &str) -> Game {
-        let re = Regex::new(r"(.*/) (?P<turn>[wb]) .* .* .* .*");
-        let g = Game::new();
-        return g;
+    fn from_fen(s: &str) -> Result<Game, Box<dyn Error>> {
+        let re = Regex::new(r"(.*/) (?P<turn>[wb]) .* .* .* .*")?;
+        let mut g = Game::new();
+        for m in re.captures_iter(s) {
+            println!("{:?}",m)
+        }
+
+        g.board.put(Location::new(3, 5), Some(Piece{pieceType: PieceType::Bishop, color: Color::Black}));
+        return Ok(g);
     }
 
     fn to_fen(&self) -> &str {
@@ -64,32 +69,54 @@ impl Game {
     }
 }
 
-struct Board {
-    pieces: [i8; 64]
+struct ArrayBoard {
+    pieces: [[Option<Piece>; 8]; 8]
 }
 
-impl Board {
-    fn at(l: Location) -> Piece {
-        Piece{pieceType: PieceType::Pawn, color: Color::Black}
+trait Board {
+    fn new() -> Self;
+    fn at(&mut self, l: Location) -> Option<Piece>;
+    fn put(&mut self, l: Location, p: Option<Piece>);
+}
+
+impl Board for ArrayBoard {
+    fn new() -> ArrayBoard {
+        return ArrayBoard{pieces: [[None; 8]; 8]}
+    }
+    fn at(&mut self, l: Location) -> Option<Piece> {
+        return self.pieces[l.x as usize][l.y as usize];
     }
 
-    fn put(l: Location, p: Piece) {
-
+    fn put(&mut self, l: Location, p: Option<Piece>) {
+        self.pieces[l.x as usize][l.y as usize] = p;
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::Game;
+    use std::error::Error;
+
+    use crate::{Game, Piece, Board, Location, ArrayBoard};
 
 
     #[test]
-    fn fen_tests() {
+    fn fen_tests() -> Result<(), Box<dyn Error>> {
         let input = "8/5k2/3p4/1p1Pp2p/pP2Pp1P/P4P1K/8/8 b - - 99 50";
-        let game = Game::from_fen(input);
+        let game = Game::from_fen(input)?;
         let output = game.to_fen();
 
-        assert_eq!(input, output)
+        assert_eq!(input, output);
+        return Ok(())
+    }
+
+    #[test]
+    fn place_test() {
+        let mut board = ArrayBoard::new();
+        let piece = Piece{pieceType: crate::PieceType::King, color: crate::Color::Black};
+        board.put(Location{x: 3, y: 4}, Some(piece));
+        assert_eq!(Some(piece), board.at(Location{x: 3, y: 4}));
+        board.put(Location{x: 3, y: 4}, None);
+        assert_eq!(None, board.at(Location{x: 3, y: 4}));
     }
 
 }
